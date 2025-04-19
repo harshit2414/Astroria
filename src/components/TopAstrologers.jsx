c6d9ac12e1702f97c3ceef101dbcfa82596fea0b
harshit2414 <harshitgarg9462@gmail.com>
2025-04-19 14:19:26 +0530

    minor changes

--- src/components/TopAstrologers.jsx
+++ src/components/TopAstrologers.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 
-const TopDoctors = () => {
+const TopAstrologers = () => {
 
     const navigate = useNavigate()
     const {doctors} = useContext(AppContext)
@@ -10,8 +10,8 @@ const TopDoctors = () => {
 
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
-        <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
-        <p className='sm:w-1/3 text-center text-sm'>Simply Browse through our extensive list of trusted doctors.</p>
+        <h1 className='text-3xl font-medium'>Top Astrologers to Book</h1>
+        <p className='sm:w-1/3 text-center text-sm'>Simply Browse through our extensive list of trusted astrologers.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0 '>
             {doctors.slice(0,10).map((item,index)=>(
                 <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
@@ -31,4 +31,4 @@ const TopDoctors = () => {
   )
 }
 
-export default TopDoctors
+export default TopAstrologers
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
-import TopDoctors from '../components/TopDoctors'
+import TopAstrologers from '../components/TopAstrologers'
 import Banner from '../components/Banner'
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
     <div>
         <Header/>
         <SpecialityMenu/>
-        <TopDoctors/>
+        <TopAstrologers/>
         <Banner/>
     </div>
   )
